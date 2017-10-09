18748b71c06ed62ee2de7972f3e2ab6a201dc08c
Raman But-Husaim <raman_but-husaim@live.com>
2017-10-09 21:36:56 +0300

    Survey: function clauses

--- survey/lib/survey/handler.ex
+++ survey/lib/survey/handler.ex
@@ -2,9 +2,12 @@ defmodule Survey.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
+
+  def log(conv), do: IO.inspect(conv)
 
   def parse(request) do
     [method, path, _] =
@@ -16,8 +19,12 @@ defmodule Survey.Handler do
     %{ method: method, path: path, resp_body: "" }
   end
 
-  def route(conv) do
+  def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{ conv | resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(%{method: "GET", path: "/bears"} = conv) do
+    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
   end
 
   def format_response(conv) do
@@ -33,6 +40,26 @@ end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+expected_response = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 20
+
+Bears, Lions, Tigers
+"""
+
+response = Survey.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
