dbb9ff03a71a09b4bb7d5525601b54d9755c8cbf
Raman But-Husaim <raman_but-husaim@live.com>
2017-10-09 21:52:39 +0300

    Survey: request params and status codes

--- survey/lib/survey/handler.ex
+++ survey/lib/survey/handler.ex
@@ -16,25 +16,49 @@ defmodule Survey.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{
+      method: method,
+      path: path,
+      resp_body: "",
+      status: nil
+    }
   end
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers" }
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-    %{ conv | resp_body: "Teddy, Smokey, Paddington" }
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(conv) do
+    %{ conv | status: 404, resp_body: "No #{conv.path} here!" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
@@ -60,6 +84,46 @@ IO.puts(response)
 
 request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+expected_response = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 20
+
+Bears, Lions, Tigers
+"""
+
+response = Survey.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+expected_response = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 20
+
+Bears, Lions, Tigers
+"""
+
+response = Survey.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+GET /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
