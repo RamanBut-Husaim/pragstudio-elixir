9fde9735fbb15fe18ca233f631e332852dce816f
Raman But-Husaim <raman_but-husaim@live.com>
2017-11-09 23:05:34 +0300

    Survey: add test for pledge server

--- survey/lib/survey/pledge_server.ex
+++ survey/lib/survey/pledge_server.ex
@@ -65,20 +65,20 @@ defmodule Survey.PledgeServer do
   end
 end
 
-alias Survey.PledgeServer
+# alias Survey.PledgeServer
 
-pid = PledgeServer.start()
+# pid = PledgeServer.start()
 
-send pid, {:stop, "hammertime"}
+# send pid, {:stop, "hammertime"}
 
-IO.inspect PledgeServer.create_pledge("larry", 10)
-IO.inspect PledgeServer.create_pledge("moe", 20)
-IO.inspect PledgeServer.create_pledge("curly", 30)
-IO.inspect PledgeServer.create_pledge("daisy", 40)
-IO.inspect PledgeServer.create_pledge("grace", 50)
+# IO.inspect PledgeServer.create_pledge("larry", 10)
+# IO.inspect PledgeServer.create_pledge("moe", 20)
+# IO.inspect PledgeServer.create_pledge("curly", 30)
+# IO.inspect PledgeServer.create_pledge("daisy", 40)
+# IO.inspect PledgeServer.create_pledge("grace", 50)
 
-IO.inspect PledgeServer.recent_pledges()
+# IO.inspect PledgeServer.recent_pledges()
 
-IO.inspect PledgeServer.total_pledged()
+# IO.inspect PledgeServer.total_pledged()
 
-IO.inspect Process.info(pid, :messages)
+# IO.inspect Process.info(pid, :messages)
---
+++ survey/test/pledge_server_test.exs
@@ -0,0 +1,19 @@
+defmodule PledgeServerTest do
+  use ExUnit.Case, async: true
+
+  alias Survey.PledgeServer
+
+  test "caches the 3 most recent pledges and totals their amounts" do
+    PledgeServer.start()
+
+    PledgeServer.create_pledge("frodo", 100)
+    PledgeServer.create_pledge("sam", 200)
+    PledgeServer.create_pledge("mary", 300)
+    PledgeServer.create_pledge("pippin", 400)
+
+    expected_pledges = [{"pippin", 400}, {"mary", 300}, {"sam", 200} ]
+
+    assert PledgeServer.recent_pledges() == expected_pledges
+    assert PledgeServer.total_pledged() == 900
+  end
+end
